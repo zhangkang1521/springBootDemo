b8e3933e3449a6f57afa621ef30855c1a3cc63e2
zhangkang <zhangkang@lvmama.com>
2017-06-26 11:38:45 +0800

    日志

--- src/main/java/org/zk/controller/UserController.java
+++ src/main/java/org/zk/controller/UserController.java
@@ -3,6 +3,8 @@ package org.zk.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,8 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     UserRepository userRepository;
 
@@ -26,6 +30,11 @@ public class UserController {
     @ApiOperation(value="获取用户列表", notes="获取用户列表，后面还有很多说明")
     @PostMapping("/list")
     public List<User> list(@RequestBody UserParam userParam){
+        logger.trace("trace");
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
 //        List<User> userList = new ArrayList<User>();
 //        User user = new User();
 //        user.setId(1L);
