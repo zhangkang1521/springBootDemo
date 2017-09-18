61ddc590121c05401c1b9fdf02e27eaa7f0011e1
zhangkang <zhangkang@lvmama.com>
2017-09-18 09:26:16 +0800

    spring boot 邮件发送

--- src/test/java/org/zk/BaseTest.java
+++ src/test/java/org/zk/BaseTest.java
@@ -1,7 +1,11 @@
 package org.zk;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -10,4 +14,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BaseTest {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    @Test
+    public void send(){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("hello");
+        message.setText("dear Jane: xxx");
+        message.setTo("zhangkang@lvmama.com");
+        message.setFrom("zhangkang_test@163.com");
+        mailSender.send(message);
+    }
 }
