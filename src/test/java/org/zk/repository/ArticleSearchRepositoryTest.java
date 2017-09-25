a688e944e472611ba55bbf23a116e078d913d778
zhangkang <zhangkang@lvmama.com>
2017-09-25 11:11:26 +0800

    精确查询

--- src/test/java/org/zk/repository/ArticleSearchRepositoryTest.java
+++ src/test/java/org/zk/repository/ArticleSearchRepositoryTest.java
@@ -1,10 +1,14 @@
 package org.zk.repository;
 
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.QueryStringQueryBuilder;
+import org.elasticsearch.index.search.MultiMatchQuery;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.zk.domain.Article;
 
@@ -24,9 +28,9 @@ public class ArticleSearchRepositoryTest {
     @Test
     public void testSave(){
         Article article = new Article();
-        article.setId(3L);
-        article.setTitle("笑傲江湖");
-        article.setContent("xxx");
+        article.setId(44L);
+        article.setTitle("射雕英雄传");
+        article.setContent("射雕英雄传xx");
         repository.save(article);
     }
 
@@ -35,6 +39,18 @@ public class ArticleSearchRepositoryTest {
         String queryString="笑";//搜索关键字
         QueryStringQueryBuilder builder = new QueryStringQueryBuilder(queryString);
         Iterable<Article> searchResult = repository.search(builder);
+        Iterator<Article> iterator = searchResult.iterator();
+        while(iterator.hasNext()){
+            Article article = iterator.next();
+            System.out.println(article);
+        }
+    }
+
+    @Test
+    public void testQueryField() {
+        QueryBuilder qb = QueryBuilders.termsQuery("id", new Integer[]{3, 4});
+        System.out.println(qb);
+        Iterable<Article> searchResult = repository.search(qb);
         Iterator<Article> iterator = searchResult.iterator();
         while(iterator.hasNext()){
             Article article = iterator.next();
