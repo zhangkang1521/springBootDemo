63f27e86c9aede424b66072949c2b7340de9597d
zhangkang1521 <zhangkang_pub@163.com>
2017-10-08 22:41:27 +0800

    cros

---
+++ src/main/java/org/zk/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+
+package org.zk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods(new String[]{HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.DELETE.name(), HttpMethod.PUT.name()});
+    }
+
+
+}
