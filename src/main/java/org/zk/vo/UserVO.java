96b1386b65c7156ba983ddf61aba1bf1e7c1fdf0
zhangkang1521 <zhangkang_pub@163.com>
2017-11-04 17:45:33 +0800

    thymeleaf

---
+++ src/main/java/org/zk/vo/UserVO.java
@@ -0,0 +1,34 @@
+package org.zk.vo;
+
+/**
+ * Created by Administrator on 11/4/2017.
+ */
+public class UserVO {
+    private Integer id;
+    private String username;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
