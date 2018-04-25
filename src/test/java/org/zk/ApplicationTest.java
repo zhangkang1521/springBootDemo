61cac09f2c22102a2d984d2b92decacccf34f99a
zhangkang <zhangkang_pub@163.com>
2018-04-25 20:29:05 +0800

    指定java版本

--- src/test/java/org/zk/ApplicationTest.java
+++ src/test/java/org/zk/ApplicationTest.java
@@ -1,5 +1,6 @@
 package org.zk;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -9,5 +10,10 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BaseTest {
+public class ApplicationTest {
+
+    @Test
+    public void test1() {
+        System.out.println("hello");
+    }
 }
