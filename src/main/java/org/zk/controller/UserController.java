9dd6a28271633c76fb3593b06f56d4642c1c34e3
zhangkang <zhangkang_pub@163.com>
2018-04-26 16:17:37 +0800

    自动配置

--- src/main/java/org/zk/controller/UserController.java
+++ src/main/java/org/zk/controller/UserController.java
@@ -2,8 +2,10 @@ package org.zk.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.zk.domain.User;
+import org.zk.hello.HelloService;
 
 /**
  * Created by zhangkang on 2017/7/31.
@@ -13,13 +15,11 @@ public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @RequestMapping("/")
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/")
     public String index(){
-        logger.trace("trace");
-        logger.debug("debug");
-        logger.info("info");
-        logger.warn("warn");
-        logger.error("error");
-        return "hello";
+        return helloService.sayHello();
     }
 }
---
+++ src/main/java/org/zk/domain/User.java
@@ -0,0 +1,16 @@
+package org.zk.domain;
+
+/**
+ * Created by Administrator on 4/26/2018.
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
