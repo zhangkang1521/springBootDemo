22ae6b43f1715a0cde2bfd41d66b07e3acb6e08f
zhangkang <zhangkang_pub@163.com>
2018-09-08 22:19:08 +0800

    测试queue-size不足

--- src/main/java/org/zk/controller/UserController.java
+++ src/main/java/org/zk/controller/UserController.java
@@ -2,8 +2,11 @@ package org.zk.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zk.domain.Article;
+import org.zk.repository.ArticleSearchRepository;
 
 /**
  * Created by zhangkang on 2017/7/31.
@@ -13,9 +16,18 @@ public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    ArticleSearchRepository repository;
+
+    int i = 0;
+
     @RequestMapping("/")
-    public String index(){
-        logger.debug("xxx");
-        return "hello";
+    public Article index(){
+        Article article = new Article();
+        String threadName = Thread.currentThread().getName();
+        article.setTitle(threadName);
+        repository.save(article);
+        logger.info("{}, {}", threadName, i++);
+        return article;
     }
 }
