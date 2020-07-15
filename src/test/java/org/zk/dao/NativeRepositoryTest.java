48148ddf302b12744c1e0f3fa259d54930483ebf
zhangkang <zhangkang@lvmama.com>
2020-07-15 17:18:17 +0800

    使用本地数据库

--- src/test/java/org/zk/dao/NativeRepositoryTest.java
+++ src/test/java/org/zk/dao/NativeRepositoryTest.java
@@ -6,12 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zk.dto.UserDto;
-import org.zk.dto.YsResultDto;
-import org.zk.entity.User;
 
 import java.util.List;
-
-import static org.junit.Assert.*;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -22,10 +18,7 @@ public class NativeRepositoryTest {
 
 	@Test
 	public void queryList() {
-		String sql = "SELECT v.id as vouchId, v.fdb_no as fdbNo " +
-				"FROM etl_gl_accvouch v ";
-//		String sql = "select id, username from tb_user";
-//		List<UserDto> list = nativeRepository.queryList(sql, null, UserDto.class);
-		List<YsResultDto> list = nativeRepository.queryList(sql, null, YsResultDto.class);
+		String sql = "select id, username from tb_user";
+		List<UserDto> list = nativeRepository.queryList(sql, null, UserDto.class);
 	}
 }
