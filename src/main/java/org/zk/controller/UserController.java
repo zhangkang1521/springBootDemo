5b2ebd848c463fb961f59cc9551dd863dd611dab
zhangkang <zhangkang@lvmama.com>
2020-08-13 10:38:11 +0800

    dubbo升级到2.7.6

--- src/main/java/org/zk/controller/UserController.java
+++ src/main/java/org/zk/controller/UserController.java
@@ -1,7 +1,12 @@
 package org.zk.controller;
 
+import com.alibaba.dubbo.config.ProtocolConfig;
+import com.alibaba.dubbo.rpc.Protocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by zhangkang on 2017/7/31.
  */
 @RestController
-public class UserController {
+public class UserController implements ApplicationContextAware {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
@@ -21,5 +26,10 @@ public class UserController {
         logger.warn("warn");
         logger.error("error");
         return "hello";
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+       // Object protocol = applicationContext.getBean("dubbo");
     }
 }
