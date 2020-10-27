1d19d9e5f098151cedf04456e1c24f1ab6cf8d3c
zhangkang <zhangkang@lvmama.com>
2020-10-27 16:16:18 +0800

    entityManager test

--- src/main/java/org/zk/service/OrderService.java
+++ src/main/java/org/zk/service/OrderService.java
@@ -6,6 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.zk.dao.UserRepository;
 import org.zk.entity.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 @Service
 public class OrderService {
 
@@ -13,12 +16,16 @@ public class OrderService {
 	UserService userService;
 	@Autowired
 	UserRepository userRepository;
+	@Autowired
+	EntityManager entityManager;
 
 	@Transactional
 	public void f1() {
-		User user = new User();
-		user.setUsername("f1");
-		userRepository.save(user);
-		userService.f2();
+//		User user = new User();
+//		user.setUsername("f1");
+//		userRepository.save(user);
+
+		Query query = entityManager.createNativeQuery("update tb_user set username = 'xx' where id = 1");
+		query.executeUpdate();
 	}
 }
