4d0ceb06504b1b10f0bf0e706ad4769ece19479f
zhangkang <zhangkang@lvmama.com>
2021-04-26 19:28:02 +0800

    指定swagger的baseURL，默认会取到nginx的配置和servlet-context

--- src/main/java/org/zk/config/Swagger2Config.java
+++ src/main/java/org/zk/config/Swagger2Config.java
@@ -1,23 +1,37 @@
 package org.zk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.PathProvider;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
 
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
 
+	@Autowired
+	ServletContext servletContext;
+
 	@Bean
 	public Docket createRestApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				// .host("www.zhangkang.com:9999")
+				.host("www.zk.com/sys/order")
+				.pathProvider(new RelativePathProvider(servletContext) {
+					@Override
+					public String getApplicationBasePath() {
+						return "/";
+					}
+				})
 				.apiInfo(apiInfo())
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("org.zk.controller"))
