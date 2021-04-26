20399f1095d06d4c16d61921d9faae439ab7912d
zhangkang <zhangkang@lvmama.com>
2021-04-26 16:59:49 +0800

    切面方式分布式锁

--- src/main/java/org/zk/controller/UserController.java
+++ src/main/java/org/zk/controller/UserController.java
@@ -1,7 +1,11 @@
 package org.zk.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by zhangkang on 2017/7/31.
  */
 @RestController
+@Api("用户服务")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
         logger.trace("trace");
         logger.debug("debug");
@@ -21,5 +26,11 @@ public class UserController {
         logger.warn("warn");
         logger.error("error");
         return "hello";
+    }
+
+    @GetMapping("test")
+    @ApiOperation("测试swagger")
+    public String test() {
+        return "ok";
     }
 }
