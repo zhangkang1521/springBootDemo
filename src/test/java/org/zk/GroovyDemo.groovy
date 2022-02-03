379d8f313804cffba49b9588ac9b23a77f732451
zhangkang <zhangkang3306@zto.com>
2022-02-03 20:35:48 +0800

    groovy test

--- src/test/java/org/zk/GroovyDemo.groovy
+++ src/test/java/org/zk/GroovyDemo.groovy
@@ -1,5 +1,8 @@
 package org.zk
 
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.JSONObject
+
 
 /**
  * @author zhangkang
@@ -8,11 +11,19 @@ package org.zk
 class GroovyDemo {
 
     String convertJson(source) {
-        return source + "xxx"
+        JSONObject jsonObject = JSON.parse(source)
+        Object aVal = jsonObject.get("a")
+
+        JSONObject result = new JSONObject()
+
+        JSONObject result1 = new JSONObject()
+        result1.put("AA", aVal)
+        result.put("A", result1)
+        return result.toJSONString()
     }
 
     static void main(args) {
-        def a = new GroovyDemo().convertJson("{}")
+        def a = new GroovyDemo().convertJson("{\"a\": 123}")
         println(a)
         println("hello, groovy!")
     }
