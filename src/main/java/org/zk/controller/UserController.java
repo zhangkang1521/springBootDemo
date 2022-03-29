dd6159b8a1833604d05464e56b0e255312249acd
zhangkang <zhangkang3306@zto.com>
2022-03-29 13:11:54 +0800

    代理方式hotswap

--- src/main/java/org/zk/controller/UserController.java
+++ src/main/java/org/zk/controller/UserController.java
@@ -1,7 +1,13 @@
 package org.zk.controller;
 
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.target.HotSwappableTargetSource;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zk.service.UserService;
@@ -12,19 +18,48 @@ import org.zk.service.impl.HotSwapUserService;
  */
 @RestController
 @RequestMapping("/user")
-public class UserController {
+public class UserController implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
 
     @Autowired
-    UserService userService;
+    @Qualifier("userServiceImpl1")
+    UserService userServiceImpl1;
+
+//    @Autowired
+//    @Qualifier("proxyFactoryBean")
+//    UserService userService;
 
     @RequestMapping("/sayHello")
     public String sayHello(){
-       return userService.sayHello();
+       // return userService.sayHello();
+        UserService userService = (UserService)applicationContext.getBean("proxyFactoryBean");
+        return userService.sayHello();
     }
 
     @RequestMapping("/swap")
-    public String swap(String bean){
-        ((HotSwapUserService)userService).swap(bean);
-        return "已经切换到"+bean;
+    public String swap(String beanName){
+        // ((HotSwapUserService)userService).swap(bean);
+        HotSwappableTargetSource hotSwappableTargetSource = (HotSwappableTargetSource)applicationContext.getBean("hotSwappableTargetSource");
+
+        hotSwappableTargetSource.swap(applicationContext.getBean(beanName));
+        return "已经切换到"+beanName;
+    }
+
+    @Bean
+    public HotSwappableTargetSource hotSwappableTargetSource() {
+        return new HotSwappableTargetSource(userServiceImpl1);
+    }
+
+    @Bean
+    public ProxyFactoryBean proxyFactoryBean(HotSwappableTargetSource hotSwappableTargetSource) {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTargetSource(hotSwappableTargetSource);
+        return proxyFactoryBean;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
