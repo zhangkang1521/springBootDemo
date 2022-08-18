9455be4a56a6e52d72f78ec493459aaa8d61ae54
zhangkang3306 <zhangkang3306@zto.com>
2022-08-18 11:01:05 +0800

    回滚

--- src/test/java/org/zk/ApplicationTest.java
+++ src/test/java/org/zk/ApplicationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import org.zk.dao.UserDao;
 import org.zk.entity.User;
 
@@ -17,6 +18,7 @@ import java.util.List;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class ApplicationTest {
 
     @Autowired
@@ -27,13 +29,13 @@ public class ApplicationTest {
     @Sql(statements = "insert into tb_user(username) values('h2test3');")
     public void test1() {
         List<User> userList = userDao.listAll();
-        Assert.assertEquals(3, userList.size());
+        Assert.assertEquals(1, userList.size());
     }
 
     @Test
     public void test2() {
         List<User> userList = userDao.listAll();
-        Assert.assertEquals(2, userList.size());
+        Assert.assertEquals(0, userList.size());
     }
 
     @Test
@@ -42,20 +44,22 @@ public class ApplicationTest {
         user.setUsername("zk");
         userDao.insert(user);
         List<User> list = userDao.listAll();
-        Assert.assertEquals("zk", list.get(2).getUsername());
+        Assert.assertEquals("zk", list.get(0).getUsername());
     }
 
     @Test
+    @Sql(statements = "insert into tb_user(id, username) values(1, 'h2test3');")
     public void update() {
         userDao.update("aaa", 1L);
-        User user = userDao.getById(1L);
-        Assert.assertEquals("aaa", user.getUsername());
+        User user2 = userDao.getById(1L);
+        Assert.assertEquals("aaa", user2.getUsername());
     }
 
     @Test
+    @Sql(statements = "insert into tb_user(username) values('h2test3');")
     public void delete() {
         userDao.deleteById(1L);
         List<User> list = userDao.listAll();
-        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(0, list.size());
     }
 }
