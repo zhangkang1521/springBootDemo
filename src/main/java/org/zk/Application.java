93f973e44b3402028b9d60be8bbb0d1e3115dbec
zhangkang3306 <zhangkang3306@zto.com>
2023-06-19 13:44:25 +0800

    apollo

--- src/main/java/org/zk/Application.java
+++ src/main/java/org/zk/Application.java
@@ -1,11 +1,10 @@
 package org.zk;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 /**
  * Created by zhangkang on 2017/1/12.
@@ -14,8 +13,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        // https://www.cnblogs.com/markLogZhu/p/12497258.html
-        // ApolloApplicationContextInitializer
+
+
+        //com.ctrip.framework.apollo.spring.boot.ApolloApplicationContextInitializer
+        // 往enviroment加入 ApolloBootstrapPropertySources ApolloPropertySources
+
+        // 自动配置
+        //com.ctrip.framework.apollo.spring.boot.ApolloAutoConfiguration
+
         SpringApplication.run(Application.class, args);
+
+        System.in.read();
+    }
+
+    @Bean
+    public CommandLineRunner runner(Environment enviroment) {
+        // configurationProperties
+        // commandLindArgs 命令行参数
+        // systemProperties 系统属性(spring)
+        // systemEnviroment 系统环境变量(spring)
+        // random 随机数
+        // application.properties
+        // demo.properties @PropertySource
+        return (String... args) -> {
+            System.out.println(enviroment);
+        };
     }
 }
--- src/main/java/org/zk/controller/UserController.java
+++ src/main/java/org/zk/controller/UserController.java
@@ -1,36 +1,36 @@
-package org.zk.controller;
-
-import com.ctrip.framework.apollo.spring.annotation.SpringValueProcessor;
-import com.ctrip.framework.apollo.spring.property.AutoUpdateConfigChangeListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.zk.config.DemoConfig;
-
-/**
- * Created by zhangkang on 2017/7/31.
- */
-@RestController
-public class UserController {
-
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-
-    /**
-     * 自动更新配置，@Value注解的才更新
-     * @see SpringValueProcessor#postProcessBeforeInitialization(java.lang.Object, java.lang.String)
-     * @see AutoUpdateConfigChangeListener#onChange(com.ctrip.framework.apollo.model.ConfigChangeEvent)
-     */
-    @Value("${timeout}")
-    private Long timeout;
-
-    @Autowired
-    private DemoConfig demoConfig;
-
-    @RequestMapping("/")
-    public String index(){
-        return timeout + ":" + demoConfig.getAa();
-    }
-}
+//package org.zk.controller;
+//
+//import com.ctrip.framework.apollo.spring.annotation.SpringValueProcessor;
+//import com.ctrip.framework.apollo.spring.property.AutoUpdateConfigChangeListener;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.zk.config.DemoConfig;
+//
+///**
+// * Created by zhangkang on 2017/7/31.
+// */
+//@RestController
+//public class UserController {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+//
+//    /**
+//     * 自动更新配置，@Value注解的才更新
+//     * @see SpringValueProcessor#postProcessBeforeInitialization(java.lang.Object, java.lang.String)
+//     * @see AutoUpdateConfigChangeListener#onChange(com.ctrip.framework.apollo.model.ConfigChangeEvent)
+//     */
+//    @Value("${timeout}")
+//    private Long timeout;
+//
+//    @Autowired
+//    private DemoConfig demoConfig;
+//
+//    @RequestMapping("/")
+//    public String index(){
+//        return timeout + ":" + demoConfig.getAa();
+//    }
+//}
